8091ee61de9bb98bbdb498a7a6e6c0ecccb774f9
Isaac3N <theisaac3n@gmail.com>
2020-12-13 09:57:06 +0100

    new templates added

--- src/myblog/urls.py
+++ src/myblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home 
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name ="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='articles'),
 ]

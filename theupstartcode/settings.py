f6f7d9ba74edcbf0d0c57d7d1fcb7052bf13e980
Isaac3N <theisaac3n@gmail.com>
2020-12-18 22:22:21 +0100

    new updates

--- theupstartcode/settings.py
+++ theupstartcode/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'theupstartprogrammer.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'ckeditor_uploader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,10 +44,13 @@ INSTALLED_APPS = [
 
     'myblog', 
     'users',
-    'ckeditor_uploader',
+    
     'ckeditor',
+
     
 ]
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
